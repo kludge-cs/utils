21b9781473a0b51b9497d49723ea6d666a862054
drainpixie <121581793+drainpixie@users.noreply.github.com>
2025-10-21 22:36:11 +0200

    build: add nix flake

---
+++ flake.nix
@@ -0,0 +1,70 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    hooks,
+  }: let
+    systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forAllSystems = f:
+      nixpkgs.lib.genAttrs systems (system:
+        f rec {
+          pkgs = nixpkgs.legacyPackages.${system};
+          inherit system;
+        });
+  in {
+    devShells = forAllSystems ({
+      pkgs,
+      system,
+    }: {
+      default = pkgs.mkShell {
+        inherit (self.checks.${system}.pre-commit) shellHook;
+
+        packages =
+          builtins.attrValues {
+            inherit (pkgs.nodePackages) nodejs pnpm typescript typescript-language-server;
+          }
+          ++ self.checks.${system}.pre-commit.enabledPackages;
+      };
+    });
+
+    checks = forAllSystems ({
+      pkgs,
+      system,
+    }: {
+      pre-commit = hooks.lib.${system}.run {
+        src = ./.;
+
+        hooks = {
+          eslint = {
+            enable = true;
+            entry = "pnpx eslint";
+            files = "\\.(ts|js|tsx|jsx)$";
+          };
+
+          prettier = {
+            enable = true;
+            excludes = ["flake.lock"];
+          };
+
+          statix = {
+            enable = true;
+            settings.ignore = ["/.direnv"];
+          };
+
+          convco.enable = true;
+          alejandra.enable = true;
+        };
+      };
+    });
+
+    formatter = forAllSystems ({pkgs, ...}: pkgs.alejandra);
+  };
+}
